8ad9b80c29e5de0bcffd5b932656707926e40c6a
Dana <dana@holbertonstudents.com>
2026-09-19 21:16:55 +0300

    Add core data structures task 3

--- python_fundamentals/core_data_structures/replace_in_list.py
+++ python_fundamentals/core_data_structures/replace_in_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i in range(len(row)):
-            if i < len(row) - 1:
-                print("{:d}".format(row[i]), end=" ")
-            else:
-                print("{:d}".format(row[i]), end="")
-        print()
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    my_list[idx] = element
+    return my_list
