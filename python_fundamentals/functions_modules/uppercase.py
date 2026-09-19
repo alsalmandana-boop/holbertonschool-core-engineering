36b2dbf6e716e1485ef45f582cc1e0625ef991be
Dana <dana@holbertonstudents.com>
2026-09-19 15:13:35 +0300

    add file and dirctory of task 2

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def uppercase(str):
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            print("{}".format(chr(ord(char) - 32)), end="")
+        else:
+            print("{}".format(char), end="")
+    print()
