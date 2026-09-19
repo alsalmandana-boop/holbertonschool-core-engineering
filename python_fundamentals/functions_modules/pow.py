0fc7ac0d1b381539cb3b6a8b44f01f84b3d22091
Dana <dana@holbertonstudents.com>
2026-09-19 15:58:06 +0300

    add file and dirctory of task 3

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -4,7 +4,12 @@
 def pow(a, b):
     result = 1
 
-    for i in range(b):
-        result *= a
+    if b >= 0:
+        for i in range(b):
+            result *= a
+    else:
+        for i in range(-b):
+            result *= a
+        result = 1 / result
 
     return result
