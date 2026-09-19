319819b05039deeb838872d409150ef5d893ea1a
Dana <dana@holbertonstudents.com>
2026-09-19 20:59:20 +0300

    Add core data structures task 3

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]))
+            else:
+                print("{:d}".format(row[i]), end=" ")
